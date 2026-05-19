5edd1bc013c75ac3a4012b0f18d703af4a2a82bd
Indri Oktavia Ramadani <indriocta555@gmail.com>
2026-05-19 09:49:43 +0700

    pesan perubahan

--- database/migrations/2026_05_19_022758_create_anggota_table.php
+++ database/migrations/2026_05_19_022758_create_anggota_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('anggota', function (Blueprint $table) {
             $table->id();
+            $table->string('kode_anggota', 20)->unique();
+            $table->string('nama', 100);
+            $table->string('email', 100)->unique();
+            $table->string('telepon', 15);
+            $table->text('alamat');
+            $table->date('tanggal_lahir');
+            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan']);
+            $table->string('pekerjaan', 50)->nullable();
+            $table->date('tanggal_daftar');
+            $table->enum('status', ['Aktif', 'Nonaktif'])->default('Aktif');
             $table->timestamps();
         });
     }
